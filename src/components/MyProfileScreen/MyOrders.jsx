ceb6e67d66e3dfc703a9ef0a4643935d9a3e8024
praveenprk <praveenkumarkp666@gmail.com>
2022-03-02 13:36:01 +0530

    Can view all orders

---
+++ src/components/MyProfileScreen/MyOrders.jsx
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import '../../styles/MyProfileScreenStyles/MyOrders.css'
+
+const MyOrders = () => {
+
+    const [orders, setOrders] = useState([])
+    
+    useEffect(() => {
+        axios.get(`https://fakestoreapi.com/products?limit=5`)
+            .then(res => {
+                setOrders(res.data)
+            })
+    },[])
+
+    console.log(orders)
+    
+    return (
+        <div className='d-flex flex-column p-4'>
+            <h4><b>My Orders</b></h4>
+            <ul>
+             {
+                orders.map((order, i) => {
+                    //can write nested return in React.
+                    return(
+                    <>
+                    <Link to={`/my-orders/${order.id}`}>
+                    <li className='orders-card' key={i}>
+                        <div className='d-flex flex-row'>
+                        <img width={50} height={50} src={order.image}/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <p>{order.title}</p>
+                        </div>
+                    </li>
+                    </Link>
+                    </>
+                    )
+                })
+            }
+            </ul>
+        </div>
+    )
+
+}
+export default MyOrders
