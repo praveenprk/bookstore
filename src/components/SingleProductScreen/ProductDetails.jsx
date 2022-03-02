c76db4edff1806b64c4566dd4f72241d93614301
praveenprk <praveenkumarkp666@gmail.com>
2022-03-02 13:37:02 +0530

    Product Desc using useContext

--- src/components/SingleProductScreen/ProductDetails.jsx
+++ src/components/SingleProductScreen/ProductDetails.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useContext } from 'react'
+import { productContext } from '../../pages/SingleProductPage'
 import '../../styles/ProductDetails.css'
 
+
 function  ProductDetails() {
+    const product = useContext(productContext)
+    // console.log(product.description)
     return (
         <div className='product-info'>
             <h3>Product Info</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ducimus iusto molestias totam eveniet voluptates quod commodi repellendus impedit illo cupiditate porro quae, quam, officiis excepturi consequatur minus fugiat similique.</p>
+            <p>{product.description}</p>
             {/* <button className='btn btn-success'>Button</button> */}
         </div>
     )
