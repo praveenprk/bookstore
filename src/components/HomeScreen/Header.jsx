9e18176f91169a034858e7d5f666d007ab8a458e
praveenprk <praveenkumarkp666@gmail.com>
2022-03-11 21:16:03 +0530

    Applied MUI Icons

--- src/components/HomeScreen/Header.jsx
+++ src/components/HomeScreen/Header.jsx
@@ -1,20 +1,24 @@
+import { HomeRepairServiceOutlined } from '@mui/icons-material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../../styles/HomeScreen/Header.css'
+import HomeIcon from '@mui/icons-material/Home'
+import LoginIcon from '@mui/icons-material/Login';
+
 
 function Header({login}) {
     return (
         <>
         <ul className='home-header d-flex'>
             <li>
-                <Link to='/'><i className="bi bi-house"></i></Link>
+                <Link to='/'><HomeIcon /></Link>
             </li>
             <li><img src='https://919kicks.com/wp-content/uploads/2021/09/IMG-20210907-WA0027-removebg-preview.png'
                     alt='919Kicks logo'
                     width={100}
                     height={100}/></li>
             <li>
-                <Link to='/login'><i className="bi bi-lock"></i></Link>
+                <Link to='/login'><LoginIcon/></Link>
             </li>
         </ul>
         </>
