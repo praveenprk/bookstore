84bb8faa22edcebae06520befe045fb3c7cac857
praveenprk <praveenkumarkp666@gmail.com>
2022-03-11 21:17:06 +0530

    Used MUI

--- src/components/HomeScreen/ProductCards.jsx
+++ src/components/HomeScreen/ProductCards.jsx
@@ -1,58 +1,46 @@
-import React from "react";
-import { Card, Row, Col, Container } from "react-bootstrap";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import "../../styles/HomeScreen/ProductCards.css";
+import productSlice, { setProductData } from "../../features/products/productSlice";
+import "../../styles/HomeScreen/ProductCards.css"
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Container from "@mui/material/Container";
+import { Chip, Grid } from "@mui/material";
 
-function ProductCards() {
-  const newArrivals = [
-    {
-      id: 1,
-      product_title: "Nike Booster",
-      price: 500,
-      image:
-        "https://919kicks.com/wp-content/uploads/2021/11/Yeezy-Logo-Website-e1637051091746.png",
-    },
-    {
-      id: 2,
-      product_title: "Nike Kids",
-      price: 600,
-      image:
-        "https://919kicks.com/wp-content/uploads/2021/11/Yeezy-Logo-Website-e1637051091746.png",
-    },
-    {
-      id: 3,
-      product_title: "Nike Casual",
-      price: 700,
-      image:
-        "https://919kicks.com/wp-content/uploads/2021/11/Yeezy-Logo-Website-e1637051091746.png",
-    },
-    {
-      id: 4,
-      product_title: "Nike Sports",
-      price: 800,
-      image:
-        "https://919kicks.com/wp-content/uploads/2021/11/Yeezy-Logo-Website-e1637051091746.png",
-    },
-    {
-      id: 5,
-      product_title: "Nike Fashion",
-      price: 900,
-      image:
-        "https://919kicks.com/wp-content/uploads/2021/11/Yeezy-Logo-Website-e1637051091746.png",
-    },
-  ];
+const ProductCards = () => {
 
-  return (
-    <Container>
-      <Row>
+  const [newArrivals, setNewArrivals] = React.useState([])
+  const dispatch = useDispatch()
+
+  useEffect(async () => {
+    await axios.get('https://fakestoreapi.com/products?limit=6')
+    // await axios.get('/products/all')
+    .then(
+      res => {
+        setNewArrivals(res.data)
+        dispatch(setProductData(res.data))
+        
+      }
+    )
+  },[])
+
+/* <Row>
         {newArrivals.map((product, i) => {
           return (
             <Col key={i} md={4} sm={6} xs={6}>
-              <Link to="/product/:id">
+              <Link to={`/product/${product.id}`}>
                 <Card>
                   <Card.Img src={product.image} />
                   <Card.Body>
-                    <Card.Title>{product.product_title}</Card.Title>
+                    <Card.Title>{product.name}</Card.Title>
+                    {product.quantity === 0 ? <Badge bg='danger'>Sold Out</Badge> : null}
+                    <hr/>
                     <Card.Text>{product.price}</Card.Text>
                   </Card.Body>
                 </Card>
@@ -60,9 +48,44 @@ function ProductCards() {
             </Col>
           );
         })}
-      </Row>
-    </Container>
-  );
+  </Row> */
+
+  return (
+  <Container>
+    <Grid container spacing={2}>
+    {
+      newArrivals.map((product,i) => (
+        <Grid item xs={6} sm={4} lg={3}>
+        <Card sx={{ maxWidth: 345 }} key={i}>
+          <CardMedia
+            component="img"
+            height="140"
+            alt={product.name}
+            image={product.image}
+            title={product.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {product.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {product.description}
+            </Typography>
+          </CardContent>
+          <CardActions display='inline' ml='10'>
+            <Button size="small" color="primary">
+              <Link to={`/products/${product.id}`}>View</Link>
+              <Chip padding={2} label={`$${product.price}`} variant="outlined"/>
+            </Button>
+          </CardActions>
+        </Card>
+        </Grid>
+      ))
+    }
+    </Grid>
+  </Container>
+  )
+  
 }
 
 export default ProductCards;
--- src/components/LoginScreen/LoginScreen.jsx
+++ src/components/LoginScreen/LoginScreen.jsx
@@ -1,10 +1,15 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Form, /*Button*/ } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
 import '../../styles/LoginScreenStyle/LoginScreenStyle.css'
-
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useDispatch } from 'react-redux'
+import { authUser, unauthUser } from '../../features/auth/authSlice'
+import { Button, Grid, Typography } from '@mui/material'
+import LoginIcon from '@mui/icons-material/Login'
 
 function LoginScreen() {
     
@@ -14,8 +19,35 @@ function LoginScreen() {
     const [login, setLogin] = useState(false)
     const [userInfo, setUserInfo] = useState({})
 
+    const dispatch = useDispatch()
+
+
+  //Error Toast definition
+    const loginError = () => {toast.error('Wrong username/password!', {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      })
+    }
+
+    //Success
+    const loginOK = () => {toast.success('Logged In... Redirecting...', {
+      position: "top-center",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      })
+    }
+
     const handleEmail = (e) => {
-        newEmail(e.target.value)
+      newEmail(e.target.value)
     }
 
     const handlePass = (e) => {
@@ -32,7 +64,7 @@ function LoginScreen() {
 
     //login user
 
-     const handleSubmit = e => {
+    const handleSubmit = e => {
     e.preventDefault()
     const loginDetails = {
         email: email,
@@ -41,7 +73,13 @@ function LoginScreen() {
     axios.post('/users/login', loginDetails)
     .then(res => {
         const { status, message } = res.data
-        status === true ? setLogin(true) : alert(message)
+        // status === true ? setLogin(true) : loginError()
+        if(status === true) {
+           loginOK()
+          setTimeout(() => setLogin(true), 2000)
+        } else {
+          loginError()
+        }
       }).catch(err => {
         console.log(err)
       })
@@ -55,6 +93,7 @@ function LoginScreen() {
           .then(res => {
             const {data} = res
             setUserInfo(data)
+            console.log(userInfo)
           }).catch(err => console.log(err))
         }
     }, [login])
@@ -63,14 +102,18 @@ function LoginScreen() {
     useEffect(() => {
         if(userInfo.name) {
             localStorage.setItem("userInfo", JSON.stringify(userInfo))
-            console.log(userInfo)
+            /* const details = JSON.parse(localStorage.getItem("userInfo"))
+            dispatch(authUser(details)) */
             window.location.href = '/'
         }
     }, [userInfo])
 
 
-  return( <div className="mt-5">
-    <legend className='text-center'>Login to shop @ 919Kicks!</legend>&nbsp;
+  return( 
+    <Grid container marginTop={5}>
+      <Grid item xs={12} lg={12} sm={12}>
+      <Typography variant="h5" component="h5" textAlign="center">
+        Login to shop @ 919Kicks!</Typography>&nbsp;
             <Form className='login-form px-5 m-4' method='post'
             onSubmit={handleSubmit}>
   <Form.Group className="mb-3">
@@ -92,14 +135,20 @@ function LoginScreen() {
     <i className='bi bi-eye showPassIcon' onClick={revealPass}></i>
   </Form.Group>
 
-  <Button variant="primary" type="submit">
+ {/*  <Button variant="primary" type="submit">
     <i className='bi bi-lock'>&nbsp;</i>Login
-  </Button>
+  </Button> */}
+  <Grid item textAlign='center'>
+  <Button variant="contained" endIcon={<LoginIcon/>} type="submit">Login</Button>
+  </Grid>
 </Form>
 <p className='text-center'>
   <Link to='/register'>Register Now</Link>
 </p>
-</div>)
+<ToastContainer/>
+</Grid>
+</Grid>
+)
 }
 
 export default LoginScreen;
