bffd47174895dab9b4e36f06a549adb148575704
praveenprk <praveenkumarkp666@gmail.com>
2022-03-11 21:15:34 +0530

    Called Organized Routes

--- src/index.js
+++ src/index.js
@@ -1,51 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
-import ReviewMain from './components/ReviewScreen/ReviewMain'
-import reportWebVitals from './reportWebVitals'
-import HomeMain from './components/HomeScreen'
-import LoginScreen from './components/LoginScreen/LoginScreen'
-import RegisterScreen from './components/LoginScreen/RegisterScreen'
-import Welcome from './pages/Welcome'
-import SingleProductPage from './pages/SingleProductPage'
-import AllProducts from './components/ProductScreens/ProductByCategory'
-import MyProfile from './pages/MyProfile'
-import MyOrderDetails from './components/MyProfileScreen/MyOrderDetails'
-import ProductByCategory from './components/ProductScreens/ProductByCategory'
-import ViewAllProducts from './components/ProductScreens/ViewAllProducts'
 
-import {store} from './app/store'
-import {Provider} from 'react-redux'
-
-let isLogged = false
-
-if(localStorage.getItem("userInfo")) {
-  isLogged = true
-}
+import {  store } from './stores/store'
+import {  Provider  } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
+import RoutesContainer from './routes/RoutesContainer'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<HomeMain />} />
-    <Route path="/product/:id" element={<SingleProductPage/>} />
-    <Route path="/add-review/:id" element={<ReviewMain />} />
-    <Route path="/login" element={<LoginScreen />} />
-    <Route path="/register" element={<RegisterScreen />} />
-    <Route path="/welcome" element={<Welcome />} />
-    <Route path="/all-products"  element={<AllProducts/>} />
-    <Route path="/profile/:id"  element={isLogged ? <MyProfile/> : <Navigate to='/login'/>} />
-    <Route path="/my-orders/:orderid"  element={<MyOrderDetails/>} />
-    <Route path="/category/:cat_name"  element={<ProductByCategory/>} />
-    <Route path="/products/all"  element={<ViewAllProducts/>} />
-    <Route path='*' element={<h1>404</h1>}/>
-    </Routes>
-    </BrowserRouter>
+    <RoutesContainer/>
+    <ToastContainer/>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-reportWebVitals()
