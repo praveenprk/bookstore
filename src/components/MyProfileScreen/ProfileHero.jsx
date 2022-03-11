13de181a7b3dae7454de91a49745b3ad0da21a9d
praveenprk <praveenkumarkp666@gmail.com>
2022-03-11 21:17:32 +0530

    Made Profile Hero Dynamic

--- src/components/MyProfileScreen/ProfileHero.jsx
+++ src/components/MyProfileScreen/ProfileHero.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 const ProfileHero = () => {
+
+  const heroDetails = useSelector(state => state.auth.userDetails)
+  console.log(heroDetails)
+  const {name, joinDate} = heroDetails
+
   return (
     <div className='container d-flex flex-column justify-content-center align-items-center mt-5' >
         <img src="https://randomuser.me/api/portraits/men/11.jpg"
         alt="profile_pic" className='rounded-circle'/>&nbsp;
-        <p style={{fontWeight: '700'}}>Praveen Kumar</p>&nbsp;
-        <span style={{fontWeight: '500'}}>Member Since: 24th Feb '22</span>
+        <p style={{fontWeight: '700'}}>{name.toUpperCase()}</p>&nbsp;
+        <span style={{fontWeight: '500'}}>Member Since: {joinDate}</span>
     </div>
-    
   )
 }
 
