d4c2eff71f08c25a02973a4e58c61af1dfff6d0b
praveenprk <praveenkumarkp666@gmail.com>
2022-03-11 21:13:33 +0530

    Commented Whole App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import './App.css'
-import CTA from './components/SingleProductScreen/CTA'
-import Header from './components/SingleProductScreen/Header'
-import ProductSingle from './components/SingleProductScreen/ProductSingle'
-import ProductTitle from './components/SingleProductScreen/ProductTitle'
-import StockReview from './components/SingleProductScreen/StockReview'
-import ProductInfoAccordion  from './components/SingleProductScreen/ProductInfoAccordion'
-import Reviews from './components/SingleProductScreen/Reviews'
-import ProductDetails from './components/SingleProductScreen/ProductDetails'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap-icons/font/bootstrap-icons.css'
+// import './App.css'
+// import CTA from './components/SingleProductScreen/CTA'
+// import Header from './components/SingleProductScreen/Header'
+// import ProductSingle from './components/SingleProductScreen/ProductSingle'
+// import ProductTitle from './components/SingleProductScreen/ProductTitle'
+// import StockReview from './components/SingleProductScreen/StockReview'
+// import ProductInfoAccordion  from './components/SingleProductScreen/ProductInfoAccordion'
+// import Reviews from './components/SingleProductScreen/Reviews'
+// import ProductDetails from './components/SingleProductScreen/ProductDetails'
+// import { Provider } from 'react-redux'
+// import { store } from './stores/store'
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header/>
-      <ProductSingle/>
-      <ProductTitle/>
-      <StockReview/>
-      <ProductDetails/>
-      <ProductInfoAccordion />
-      <Reviews/>
-      <CTA/>
-    </div>
-  )
-}
 
-export default App
+// const App = () => {
+//   return (
+//     <Provider store={store}>
+//     <div className="App">
+//       <Header/>
+//       <ProductSingle/>
+//       <ProductTitle/>
+//       <StockReview/>
+//       <ProductDetails/>
+//       <ProductInfoAccordion />
+//       <Reviews/>
+//       <CTA/>
+//     </div>
+//     </Provider>
+//   )
+// }
+
+// export default App
