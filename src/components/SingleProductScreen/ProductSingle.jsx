b920fc3c574e64118fdd6f5049937590d4e2b73c
praveenprk <praveenkumarkp666@gmail.com>
2022-03-12 22:06:36 +0530

    Removed Comment

--- src/components/SingleProductScreen/ProductSingle.jsx
+++ src/components/SingleProductScreen/ProductSingle.jsx
@@ -1,15 +1,14 @@
-import { useContext } from 'react';
-import { productContext } from '../../pages/SingleProductPage';
+import { useContext } from 'react'
+import { productContext } from '../../pages/SingleProductPage'
 import '../../styles/ProductSingle.css'
 
 const ProductSingle = () => {
+
   const productImage = useContext(productContext)
   
   return (
-      <>
+    <>
     <div className="product-single-image">
-      {/* <img className='product-image' src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8" alt=""/>
-       */}
        <img
        className='product-image'
        src={`${productImage.image}`}
@@ -19,4 +18,4 @@ const ProductSingle = () => {
   );
 }
 
-export default ProductSingle;
+export default ProductSingle
